9d4e884ffbe4975b09d4531c2ded0279ddb1e202
Luis Padilla de Jesús <luispad03@hotmail.com>
2022-09-22 03:37:03 -0500

    carpeta OBTIENEHW

---
+++ OBTIENEHW/Form1.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lInfoSistemaOperativo = New System.Windows.Forms.TextBox()
+        Me.lInfoVersionSistemaOperativo = New System.Windows.Forms.TextBox()
+        Me.lInfoNumeroCoresCPU = New System.Windows.Forms.TextBox()
+        Me.lInfoArquitecturaCPU = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lInfoNumeroCoresCPUF = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lInfoSistemaOperativo
+        '
+        Me.lInfoSistemaOperativo.Enabled = False
+        Me.lInfoSistemaOperativo.Location = New System.Drawing.Point(106, 15)
+        Me.lInfoSistemaOperativo.Name = "lInfoSistemaOperativo"
+        Me.lInfoSistemaOperativo.Size = New System.Drawing.Size(202, 20)
+        Me.lInfoSistemaOperativo.TabIndex = 0
+        '
+        'lInfoVersionSistemaOperativo
+        '
+        Me.lInfoVersionSistemaOperativo.Enabled = False
+        Me.lInfoVersionSistemaOperativo.Location = New System.Drawing.Point(106, 41)
+        Me.lInfoVersionSistemaOperativo.Name = "lInfoVersionSistemaOperativo"
+        Me.lInfoVersionSistemaOperativo.Size = New System.Drawing.Size(202, 20)
+        Me.lInfoVersionSistemaOperativo.TabIndex = 0
+        '
+        'lInfoNumeroCoresCPU
+        '
+        Me.lInfoNumeroCoresCPU.Enabled = False
+        Me.lInfoNumeroCoresCPU.Location = New System.Drawing.Point(106, 90)
+        Me.lInfoNumeroCoresCPU.Name = "lInfoNumeroCoresCPU"
+        Me.lInfoNumeroCoresCPU.Size = New System.Drawing.Size(202, 20)
+        Me.lInfoNumeroCoresCPU.TabIndex = 0
+        '
+        'lInfoArquitecturaCPU
+        '
+        Me.lInfoArquitecturaCPU.Enabled = False
+        Me.lInfoArquitecturaCPU.Location = New System.Drawing.Point(106, 116)
+        Me.lInfoArquitecturaCPU.Name = "lInfoArquitecturaCPU"
+        Me.lInfoArquitecturaCPU.Size = New System.Drawing.Size(202, 20)
+        Me.lInfoArquitecturaCPU.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(31, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "S.O.:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 44)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(85, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "PROCESADOR:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 93)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(89, 13)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "NUCLEOS LOG.:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 120)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(71, 13)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "FREC. (GHz):"
+        '
+        'lInfoNumeroCoresCPUF
+        '
+        Me.lInfoNumeroCoresCPUF.Enabled = False
+        Me.lInfoNumeroCoresCPUF.Location = New System.Drawing.Point(106, 65)
+        Me.lInfoNumeroCoresCPUF.Name = "lInfoNumeroCoresCPUF"
+        Me.lInfoNumeroCoresCPUF.Size = New System.Drawing.Size(202, 20)
+        Me.lInfoNumeroCoresCPUF.TabIndex = 0
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 68)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(83, 13)
+        Me.Label5.TabIndex = 1
+        Me.Label5.Text = "NUCLEOS FIS.:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(321, 150)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lInfoArquitecturaCPU)
+        Me.Controls.Add(Me.lInfoNumeroCoresCPU)
+        Me.Controls.Add(Me.lInfoNumeroCoresCPUF)
+        Me.Controls.Add(Me.lInfoVersionSistemaOperativo)
+        Me.Controls.Add(Me.lInfoSistemaOperativo)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "OBTIENE DATOS PC"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lInfoSistemaOperativo As TextBox
+    Friend WithEvents lInfoVersionSistemaOperativo As TextBox
+    Friend WithEvents lInfoNumeroCoresCPU As TextBox
+    Friend WithEvents lInfoArquitecturaCPU As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lInfoNumeroCoresCPUF As TextBox
+    Friend WithEvents Label5 As Label
+End Class
---
+++ OBTIENEHW/Form1.vb
@@ -0,0 +1,66 @@
+﻿Imports System.Management
+
+Imports System.Threading
+Imports System.Xml
+Imports System.Environment
+Imports Microsoft.VisualBasic.Devices
+'Imports ClassLibrary1
+Imports Microsoft.Win32
+Imports Microsoft.SqlServer
+Imports Microsoft.CSharp
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'ByVal sender As System.Object,
+        '      ByVal e As System.EventArgs) Handles MyBase.Load
+        ' Obtener información del sistema operativo
+        Dim consultaSQLSO As String = "SELECT * FROM Win32_OperatingSystem"
+        Dim objSO As New ManagementObjectSearcher(consultaSQLSO)
+        For Each info As ManagementObject In objSO.Get()
+            lInfoSistemaOperativo.Text =
+                info.Properties("Caption").Value.ToString().Trim()
+            'lInfoVersionSistemaOperativo.Text =
+            '    info.Properties("Version").Value.ToString() &
+            '    " Service Pack " &
+            '    info.Properties("ServicePackMajorVersion").Value.ToString() & "." &
+            'info.Properties("ServicePackMinorVersion").Value.ToString()
+        Next info
+
+
+        ' obtener nombre procesador 
+        Dim consultaSQLArquitectura2 As String = "SELECT * FROM Win32_Processor"
+        Dim objArquitectura2 As New ManagementObjectSearcher(consultaSQLArquitectura2)
+        For Each info As ManagementObject In objArquitectura2.Get()
+            lInfoVersionSistemaOperativo.Text =
+                info.Properties("Name").Value.ToString()   'ProcessorId      Caption      Family
+        Next info
+
+        ' Obtener número de cores del procesador (nucleos)
+        Dim consultaSQLCores As String = "SELECT * FROM Win32_Processor"
+        Dim objCores As New ManagementObjectSearcher(consultaSQLCores)
+        For Each info As ManagementObject In objCores.Get()
+            lInfoNumeroCoresCPUF.Text =
+                info.Properties("NumberOfCores").Value.ToString()
+        Next info
+
+        ' Obtener número de cores del procesador (hilos)
+        Dim consultaSQLCores2 As String = "SELECT * FROM Win32_ComputerSystem"
+        Dim objCores2 As New ManagementObjectSearcher(consultaSQLCores2)
+        For Each info As ManagementObject In objCores2.Get()
+            lInfoNumeroCoresCPU.Text =
+                info.Properties("NumberOfLogicalProcessors").Value.ToString()
+        Next info
+
+        ' obtener arquitectura procesador (GHz)
+        Dim consultaSQLArquitectura As String = "SELECT * FROM Win32_Processor"
+        Dim objArquitectura As New ManagementObjectSearcher(consultaSQLArquitectura)
+        For Each info As ManagementObject In objArquitectura.Get()
+            lInfoArquitecturaCPU.Text =
+                (info.Properties("CurrentClockSpeed").Value / 1000).ToString()
+        Next info
+
+
+
+
+    End Sub
+End Class
