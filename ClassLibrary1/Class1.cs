7531cc4a76bd5bbf324b8fbf65c01657ede07bb8
Luis Padilla de Jesús <luispad03@hotmail.com>
2022-09-22 03:37:56 -0500

    carpeta ClassLibrary

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.IO;
+using System.Management;
+
+namespace ClassLibrary1
+{
+    class Programa_ejemplo
+    {
+        static void Main(string[] args)
+        {
+
+            ManagementObjectSearcher searcher = new
+            ManagementObjectSearcher("SELECT * FROM Win32_Processor");
+            List<string> listProcessor = new List<string>();
+            foreach (ManagementObject wmi_HD in searcher.Get())
+            {
+                listProcessor.Add(wmi_HD["ProcessorID"].ToString());
+            }
+            //En listProcessor tenemos todos los procesadores del equipo en que se está ejecutando el software
+            //Por ejemplo en listProcessor[0] tenemos el id de la primera CPU de nuestra máquina.
+        }
+    }
+}
